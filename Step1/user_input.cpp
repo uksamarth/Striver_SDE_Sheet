d927ae12aa4ab868bb3b36896d83777678e71cfc
U K Samarth <01fe21bec126@kletech.ac.in>
2024-07-08 23:37:48 +0530

    user_inut file added

---
+++ Step1/user_input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x;
+    int y;
+    cout << "Enter the value of x and y: ";
+    cin >> x ;
+    cin >> y;
+    cout << "Value of x: " << x << " and y: " <<y;
+    return 0;
+}
