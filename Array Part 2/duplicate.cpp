85fc4b2a8e56d8fcd2debe9013255d905817e40d
U K Samarth <01fe21bec126@kletech.ac.in>
2024-07-12 22:37:48 +0530

    update

--- Array Part 2/duplicate.cpp
+++ Array Part 2/duplicate.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int findDuplicate(vector<int>& arr){
     sort(arr.begin(),arr.end());
-    for(int i=0;i<arr.size();i++){
+    for(int i=0;i<arr.size()-1;i++){
         if(arr[i]==arr[i+1]){
             return arr[i];
         }
